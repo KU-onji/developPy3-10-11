eaa61cfaade1fdde8bfaf0a9d93b278fd951d0d0
KU-onji <mgr_127@yahoo.co.jp>
2023-05-14 18:57:45 +0900

    My first commit!

---
+++ hello.py
@@ -0,0 +1,12 @@
+print("Hello, world!")
+
+# fmt: off
+matrix = {
+    0, 1, 2,
+    3, 4, 5,
+    6, 7, 8,
+}
+# fmt: on
+
+# use "# noqa" to ignore a line
+url = "www.google.com/search?sxsrf=APwXEdfuyoM0rcSyimvH9m4XNaNazNzRTg:1683904801277&q=Flake8+max-line-length&sa=X&ved=2ahUKEwiapI-civD-AhXqglYBHZ9kBJcQ1QJ6BAg0EAE&biw=958&bih=919&dpr=1"  # noqa
